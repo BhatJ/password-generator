1e9b448f199068d057aae1c585744e18c6d9a7d2
BhatJ <jisha2209@gmail.com>
2022-12-01 19:20:03 +1100

    Added arrays for each character type

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,18 @@
 // Assignment code here
 
+// Add arrays for each character type
+
+// Special characters
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+
+// Numerical characters
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Lowercase characters
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// Uppercase characters
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
